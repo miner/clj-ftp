6b8a742b62b9ba0d23905fd05e841e39d0f30711
Christian Blunden <christian.blunden@gmail.com>
2012-09-04 11:19:49 +0100

    update org name of project to clojars group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ftp "0.1.1"
+(defproject org.clojars.christianblunden/clj-ftp "0.1.1"
   :description "Clojure wrapper on Apache Commons Net for FTP"
   :url "https://github.com/forward/clj-ftp"
   :license {:name "Eclipse Public License"
