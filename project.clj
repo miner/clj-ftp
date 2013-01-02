303c9e03dbe8746be3a1bf00a404748631edd062
Steve Miner <steveminer@gmail.com>
2013-01-01 19:22:49 -0500

    updated lib

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject clj-ftp "0.1.2"
+(defproject clj-ftp "0.1.3"
   :description "Clojure wrapper on Apache Commons Net for FTP"
   :url "http://github.com/miner/clj-ftp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [fs "1.3.2"]
-                 [commons-net "3.1"]])
+                 [fs "1.3.3"]
+                 [commons-net "3.1"]
+                 ;;[commons-net "20030805.205232"]
+                 ])
