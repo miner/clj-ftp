5485937b8573900740883aad35114df9f92fedd7
Steve Miner <steveminer@gmail.com>
2013-02-23 10:52:25 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/clj-ftp "0.1.4"
+(defproject com.velisco/clj-ftp "0.1.5"
   :description "Clojure wrapper on Apache Commons Net for FTP"
   :url "http://github.com/miner/clj-ftp"
   :license {:name "Eclipse Public License"
