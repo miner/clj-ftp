f20bf59fa60b4f0c699326c65179f7c94991cb06
Steve Miner <steveminer@gmail.com>
2017-04-27 09:26:37 -0400

    updated deps

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject com.velisco/clj-ftp "0.3.9-SNAPSHOT"
+(defproject com.velisco/clj-ftp "0.3.9"
   :description "Clojure wrapper on Apache Commons Net for FTP"
   :url "http://github.com/miner/clj-ftp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]
-                 [commons-net "3.5"]]
+                 [commons-net "3.6"]]
   :profiles {:test {:resource-paths ["test-resources"]
-                    :dependencies [[digest "1.4.4"]]} })
+                    :dependencies [[digest "1.4.5"]]} })
 
 
