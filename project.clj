2d61ea27fc4a1a6285ef94a4fe8ce045434f3154
Steve Miner <steveminer@gmail.com>
2021-02-16 09:00:41 -0500

    bump libs, add strictReplyParsing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/clj-ftp "0.3.14"
+(defproject com.velisco/clj-ftp "0.3.15"
   :description "Clojure wrapper on Apache Commons Net for FTP"
   :url "http://github.com/miner/clj-ftp"
   :license {:name "Eclipse Public License"
@@ -6,10 +6,10 @@
   :min-lein-version "2.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]
-                 [commons-net "3.6"]]
+                 [commons-net "3.7.2"]]
   :profiles {:test {:resource-paths ["test-resources"]
                     :dependencies [[org.mockftpserver/MockFtpServer "2.7.1"]
                                    [org.slf4j/slf4j-jdk14 "1.7.30"]
-                                   [digest "1.4.9"]]}})
+                                   [digest "1.4.10"]]}})
 
 
